8fb37568b1248d899bde3ccfb63ccecbc7949f54
Manzi Asifiwe <50745253+vsifiwe@users.noreply.github.com>
2021-12-16 01:06:01 +0200

    Fix mixture of axios and jwt_decode

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import Header from './layout/Header'
 import Footer from './layout/Footer'
-import axios from 'axios'
+import jwt_decode from 'jwt_decode'
 import { Link, useNavigate } from 'react-router-dom'
 
 
